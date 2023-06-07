606d89fdcdbc9606c337c783f973a4574b0d29f4
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2023-06-07 19:51:42 +0300

    commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,31 @@
 import React, {useEffect, useState} from 'react';
-// import Auth from "../widgets/components/auth/auth";
+import Auth from "../widgets/components/auth/auth";
 import '../shared/assets/styles/reset.scss';
 import '../shared/assets/styles/globals.scss';
 import Home from "../pages/home";
-// import {useAppSelector} from "../hooks/useReduser";
+import {useAppSelector} from "../hooks/useReduser";
 
 
 function App() {
 
-    // const {isAuth} = useAppSelector(state => state.authReducer)
+    const {isAuth} = useAppSelector(state => state.authReducer)
 
-    // const [id, setId] = useState(localStorage.getItem('idInstance'))
-    // const [token, setToken] = useState(localStorage.getItem('apiTokenInstance'))
+    const [id, setId] = useState(localStorage.getItem('idInstance'))
+    const [token, setToken] = useState(localStorage.getItem('apiTokenInstance'))
 
-    // useEffect(() => {
-    //     setId(localStorage.getItem('idInstance'))
-    //     setToken(localStorage.getItem('apiTokenInstance'))
-    // }, [isAuth])
+    useEffect(() => {
+        setId(localStorage.getItem('idInstance'))
+        setToken(localStorage.getItem('apiTokenInstance'))
+    }, [isAuth])
 
     return (
         <div>
-            <Home/>
-            {/*{*/}
-            {/*    (token && id) ?*/}
-            {/*        <Home/>*/}
-            {/*        : <Auth/>*/}
-            {/*}*/}
+
+            {
+                (token && id) ?
+                    <Home/>
+                    : <Auth/>
+            }
         </div>
     );
 }
